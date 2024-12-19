9b0da45704ce02b6f9d828b0afa1bb57b25b7145
Trần Thanh Nhân <nhancmvn12@gmail.com>
2024-12-19 22:44:35 +0700

    Config docker, elasticsearch in cloud

--- src/ProductService.Api/Program.cs
+++ src/ProductService.Api/Program.cs
@@ -1,14 +1,30 @@
 using ProductService.Api.Extentions;
 using Serilog;
+using Serilog.Debugging;
+using Serilog.Formatting.Elasticsearch;
+using Serilog.Sinks.Elasticsearch;
 
 try
 {
     var builder = WebApplication.CreateBuilder(args);
- 
     Log.Logger = new LoggerConfiguration()
        .ReadFrom.Configuration(builder.Configuration)
+       .WriteTo.Elasticsearch(
+            new ElasticsearchSinkOptions(
+               new Uri("https://product-c309be.es.ap-southeast-1.aws.elastic.cloud"))
+               {
+                   ApiKey = "MUJaMzM1TUJ0TUVhM0tlTWVzRkE6LUdjZWU1RlpRLU9MekN4VkxVeTh0UQ==",
+                   AutoRegisterTemplate = true,
+                   AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+                   IndexFormat = "product-service-{0:yyyy.MM.dd}",
+                   CustomFormatter = new ExceptionAsObjectJsonFormatter(renderMessage: true),
+                   EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
+                                      EmitEventFailureHandling.WriteToFailureSink |
+                                      EmitEventFailureHandling.RaiseCallback,
+               })
        .CreateLogger();
 
+    SelfLog.Enable(msg => Log.Information(msg));
     Log.Information("Starting server.");
 
     var app = builder
