31e9e8f1f84dc98146ea74ef4413851dba2f6aa6
Nhân Trần Thanh <nhancmvn12@gmail.com>
2025-01-19 16:09:43 +0700

    Feat: notification mediator

--- src/ProductService.Application/Features/Products/Commands/Create/CreateProductCommandHandler.cs
+++ src/ProductService.Application/Features/Products/Commands/Create/CreateProductCommandHandler.cs
@@ -1,70 +1,35 @@
 ﻿using MediatR;
+using ProductService.Application.Features.Products.Notifications;
 using ProductService.Domain.Products;
-using Microsoft.AspNetCore.Http;
-using Hangfire;
-using ProductService.Application.Services.S3;
-using Amazon.S3.Model;
-using SharedLibrary.Helpers;
 
 namespace ProductService.Application.Features.Products.Commands.Create;
 
 public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, Guid>
 {
+    private readonly IMediator _mediatR;
     private readonly IProductRepository _repository;
     private readonly ProductManager _manager;
-    private readonly IS3Service _s3Service;
     public CreateProductCommandHandler(IProductRepository repository,
                                        ProductManager manager,
-                                       IS3Service s3Service)
+                                       IMediator mediatR)
     {
         _repository = repository;
         _manager = manager;
-        _s3Service = s3Service;
+        _mediatR = mediatR;
     }
     public async Task<Guid> Handle(CreateProductCommand command,
                                    CancellationToken cancellationToken)
     {
         var input = command.Model;
         var product = await _manager.CreateAsync(
-                input.Name,
-                input.Code,
-                input.Note,
-                input.CostPrice
-            );
+                                input.Name,
+                                input.Code,
+                                input.Note,
+                                input.CostPrice
+                            );
+
+        await _mediatR.Publish(new ProductCreatedNotification(input, product));
         await _repository.AddAsync(product);
-        if (input.Images != null)
-        {
-            await HandleTempFiles(input.Images, input.Code);
-        }
-
         return product.Id;
-    }
-    private async Task HandleTempFiles(IEnumerable<IFormFile> inputImages, string code)
-    {
-        var bucketName = Constants.ProductService.BucketName;
-        var prefix = $"Products/Product-{code}";
-        foreach (var image in inputImages)
-        {
-            if (!image.ContentType.StartsWith("image/"))
-            {
-                throw new Exception("Invalid File Format. We support only Images.");
-            }
-            using var memoryStream = new MemoryStream();
-            await image.CopyToAsync(memoryStream);
-            var imageData = memoryStream.ToArray();
-            //
-            var fileName = image.FileName;
-            var key = string.IsNullOrEmpty(prefix) ? fileName : $"{prefix?.TrimEnd('/')}/{fileName}";
-            var request = new PutObjectRequest()
-            {
-                BucketName = bucketName,
-                Key = key,
-                InputStream = image.OpenReadStream(),
-            };
-            request.Metadata.Add("Content-Type", image.ContentType);
-            BackgroundJob.Enqueue<IS3Service>(service => service.UploadToS3BucketAsync(bucketName, key, imageData, image.ContentType));
-        }
-
-        await Task.CompletedTask;
     }
 }
---
+++ src/ProductService.Application/Features/Products/Notifications/UploadImagesToS3Handler.cs
@@ -0,0 +1,33 @@
+﻿using Hangfire;
+using MediatR;
+using ProductService.Application.Services.S3;
+using SharedLibrary.Helpers;
+
+namespace ProductService.Application.Features.Products.Notifications;
+
+public class UploadImagesToS3Handler : INotificationHandler<ProductCreatedNotification>
+{
+    public async Task Handle(ProductCreatedNotification notification, CancellationToken cancellationToken)
+    {
+        var bucketName = Constants.ProductService.BucketName;
+        var prefix = $"Products/Product-{notification.model.Code}";
+        notification.entity.Images = new List<string>();
+
+        foreach (var image in notification.model.Images)
+        {
+            if (!image.ContentType.StartsWith("image/"))
+            {
+                throw new Exception("Invalid File Format. We support only Images.");
+            }
+            using var memoryStream = new MemoryStream();
+            await image.CopyToAsync(memoryStream);
+            var imageData = memoryStream.ToArray();
+            var fileName = image.FileName;
+            var key = string.IsNullOrEmpty(prefix) ? fileName : $"{prefix?.TrimEnd('/')}/{fileName}";
+            notification.entity.Images.Add($"{bucketName}/{key}");
+
+            BackgroundJob.Enqueue<IS3Service>(service => 
+                        service.UploadToS3BucketAsync(bucketName, key, imageData, image.ContentType));
+        }
+    }
+}
