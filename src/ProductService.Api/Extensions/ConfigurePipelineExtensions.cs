986433f6559282952775dfc9cde9d0c224d98438
nhanne <nhancmvn12@gmail.com>
2025-06-13 15:55:45 +0700

    Refactor: modify route

--- src/ProductService.Api/Extensions/ConfigurePipelineExtensions.cs
+++ src/ProductService.Api/Extensions/ConfigurePipelineExtensions.cs
@@ -49,14 +49,14 @@ internal static partial class HostingExtensions
     }
     private static WebApplication ConfigureDevelopment(this WebApplication app)
     {
+        using var scope = app.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        if (context.Database.GetPendingMigrations().Any())
+        {
+            context.Database.Migrate();
+        }
         if (!app.Environment.IsProduction())
         {
-            using var scope = app.Services.CreateScope();
-            var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-            if (context.Database.GetPendingMigrations().Any())
-            {
-                context.Database.Migrate();
-            }
             app.UseSwagger();
             app.UseSwaggerUI(options =>
             {
